8a3a89ca7ec531e03c195fb0d818c649c9826085
hyzboy <hyzboy@gmail.com>
2021-04-02 14:24:40 +0800

    flip V coords

--- app/src/main/java/com/android/gl2player/QuadUV.java
+++ app/src/main/java/com/android/gl2player/QuadUV.java
@@ -3,10 +3,10 @@ package com.android.gl2player;
 public class QuadUV extends GL2FloatBuffer{
 
     private static final float QuadUVData[] = {
-            0.0f, 0.0f,
-            1.0f, 0.0f,
             0.0f, 1.0f,
-            1.0f, 1.0f};
+            1.0f, 1.0f,
+            0.0f, 0.0f,
+            1.0f, 0.0f};
 
     QuadUV()
     {
